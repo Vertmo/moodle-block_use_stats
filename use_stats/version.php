2344697ed0a40756154adeacd08a5a02600bd06e
Vertmo <basile.pesin@gmail.com>
2017-07-07 14:57:51 +0200

    Updated version # and removed original pro directory

--- use_stats/version.php
+++ use_stats/version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017022102;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017070700;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016120500;        // Requires this Moodle version.
 $plugin->component = 'block_use_stats'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
